bb6ca89ce183bb4fc8ea56a7f995cb86ffea96b3
jacksonmclean326 <jacksonmclean326@gmail.com>
2026-01-26 19:58:46 +0000

    database

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,5 +1,32 @@
+import { object } from 'zod';
+import { prisma } from '../server';
 import { defineOptions, SeedArguments } from './types';
 
 export const options = defineOptions({});
 
-export async function seed(args?: SeedArguments) {}
+export async function seed(args?: SeedArguments) {
+  await prisma.user.createMany({
+    data: [
+      {
+        accountType: 'Student',
+        firstName: 'Jackson',
+        lastName: 'McLean',
+        netId: 'jcmsa',
+        id: 'student',
+        preferredFirstName: 'jackson',
+        preferredLastName: 'McLean',
+        roles: ['user'],
+      },
+      {
+        accountType: 'Employee',
+        firstName: 'Jeff',
+        lastName: 'Bezos',
+        netId: 'jeffds',
+        id: 'employee',
+        preferredFirstName: 'jeff',
+        preferredLastName: 'bezos',
+        roles: ['admin'],
+      },
+    ],
+  });
+}
