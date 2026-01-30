98b33c6856babae31356b26f763874e67b1bffcc
jacksonmclean326 <jacksonmclean326@gmail.com>
2026-01-30 21:47:11 +0000

    stuff

---
+++ src/app/pages/tasks/tasks.page.ts
@@ -0,0 +1,27 @@
+import { Component, inject, signal } from '@angular/core';
+import { TRPC_CLIENT } from '../../utils/trpc.client';
+import { trpcResource } from '@fhss-web-team/frontend-utils';
+
+@Component({
+  selector: 'app-tasks',
+  imports: [],
+  templateUrl: './tasks.page.html',
+  styleUrl: './tasks.page.scss',
+})
+export class TasksPage {
+  private readonly trpc = inject(TRPC_CLIENT);
+
+  protected readonly pageSize = 12;
+  private readonly pageOffset = signal(0);
+
+  protected readonly taskResource = trpcResource(
+    this.trpc.tasks.getTasksByUser.mutate,
+    () => {
+      return {
+        pageOffset: this.pageOffset(),
+        pageSize: this.pageSize,
+      };
+    },
+    { autoRefresh: true }
+  );
+}
